61d1fcea99968b698dfd91418f45b3942f99d0fa
Franco <fran14korn@live.com>
2023-06-29 11:00:35 -0300

    changes

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
 # Ruta al archivo .env
-dotenv_path = os.path.join(os.path.dirname(__file__), 'app/.env')
+#dotenv_path = os.path.join(os.path.dirname(__file__), 'app/.env')
 
 # Cargar las variables de entorno desde el archivo .env
-load_dotenv(dotenv_path)
+#load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
